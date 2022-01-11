5eea547d85f5bbab346a48768968bd44136d8274
harisankar <harisankar9633@gmail.com>
2022-01-11 20:48:28 +0530

    initial commit-6

--- static/website/tuttuscalebody.js
+++ static/website/tuttuscalebody.js
@@ -1,3 +1,6 @@
-      for ( let i = 0; i < 1000; i++ ) {
-        document.getElementById("b1").click();
+for ( let i = 0; i < 1; i++ ) {
+        document.getElementById("colour").click();
     }
+for ( let id = 0; id < 1; id++ ){
+
+}
